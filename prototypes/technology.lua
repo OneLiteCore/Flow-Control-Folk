0cd25d7b208eeb452da5ff6e1ca386a444a7227d
OneLiteCore <one.lite.core@gmail.com>
2024-03-14 01:43:15 +0800

    Fix path issue

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -14,7 +14,7 @@ data:extend ({
 {
   type = "technology",
   name = "flow_control_valves_tech",
-  icon = "__Flow Control__/graphics/technology/flow_control_valves_tech.png",
+  icon = "__Flow-Control-Folk__/graphics/technology/flow_control_valves_tech.png",
   icon_size = 256,
   effects = {
     {
